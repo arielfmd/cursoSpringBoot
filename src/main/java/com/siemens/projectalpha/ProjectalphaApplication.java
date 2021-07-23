cccffe53dfc0ea83d75b90984f697601bc77ddd0
arielfmd <ariel.f.m.domingues@gmail.com>
2021-07-23 16:47:36 -0300

    Projeto Alpha criado

---
+++ src/main/java/com/siemens/projectalpha/ProjectalphaApplication.java
@@ -0,0 +1,13 @@
+package com.siemens.projectalpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectalphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectalphaApplication.class, args);
+	}
+
+}
