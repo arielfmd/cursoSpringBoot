4eaa53cb639afb417f0ea821187f187e64f1e926
arielfmd <ariel.f.m.domingues@gmail.com>
2021-07-29 12:26:38 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/siemens/projectalpha/services/CategoriaService.java
+++ src/main/java/com/siemens/projectalpha/services/CategoriaService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import com.siemens.projectalpha.domain.Categoria;
 import com.siemens.projectalpha.repositories.CategoriaRepository;
+import com.siemens.projectalpha.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                    "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
     
 }
