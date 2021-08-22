4692c5c4376cb50d678357d3dad829ed72ab1870
arielfmd <ariel.f.m.domingues@gmail.com>
2021-08-22 17:00:29 -0300

    Endpoint /clientes/{id} disponível

---
+++ src/main/java/com/siemens/projectalpha/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.siemens.projectalpha.services;
+
+import java.util.Optional;
+
+import com.siemens.projectalpha.domain.Cliente;
+import com.siemens.projectalpha.repositories.ClienteRepository;
+import com.siemens.projectalpha.services.exceptions.ObjectNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+    
+    @Autowired
+    private ClienteRepository repo;
+
+    public Cliente buscar(Integer id) {
+        Optional<Cliente> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+            "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+    }
+}
