63625916acf690db0440b20349b7c6296250afa6
arielfmd <ariel.f.m.domingues@gmail.com>
2021-09-08 16:28:07 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/siemens/projectalpha/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.siemens.projectalpha.services;
+
+import java.util.Optional;
+
+import com.siemens.projectalpha.domain.Pedido;
+import com.siemens.projectalpha.repositories.PedidoRepository;
+import com.siemens.projectalpha.services.exceptions.ObjectNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoService {
+    
+    @Autowired
+    private PedidoRepository repo;
+
+    public Pedido buscar(Integer id) {
+        Optional<Pedido> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+            "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+    }
+}
