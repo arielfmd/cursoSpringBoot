20ad841bb45ba1509c5207530b6042097f25b1dc
arielfmd <ariel.f.m.domingues@gmail.com>
2022-03-02 16:13:04 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/siemens/projectalpha/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/siemens/projectalpha/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import com.siemens.projectalpha.domain.Cliente;
 import com.siemens.projectalpha.domain.enums.TipoCliente;
 import com.siemens.projectalpha.dto.ClienteNewDTO;
+import com.siemens.projectalpha.repositories.ClienteRepository;
 import com.siemens.projectalpha.resources.exceptions.FieldMessage;
 import com.siemens.projectalpha.services.validation.utils.BR;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository repo;
+
     @Override
     public void initialize(ClienteInsert ann) {
     }
@@ -26,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if (aux != null) {
+            list.add(new FieldMessage("email", "Email já existente"));
         }
 
         for (FieldMessage e : list) {
