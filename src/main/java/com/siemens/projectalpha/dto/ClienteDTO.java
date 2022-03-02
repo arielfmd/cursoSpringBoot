4912b67b630ff46b1c1b57a4b419798574fe2a40
arielfmd <ariel.f.m.domingues@gmail.com>
2022-03-02 16:32:52 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/siemens/projectalpha/dto/ClienteDTO.java
+++ src/main/java/com/siemens/projectalpha/dto/ClienteDTO.java
@@ -6,9 +6,11 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 import com.siemens.projectalpha.domain.Cliente;
+import com.siemens.projectalpha.services.validation.ClienteUpdate;
 
 import org.hibernate.validator.constraints.Length;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
     private static final long serialVersionUID = 1L;
     
