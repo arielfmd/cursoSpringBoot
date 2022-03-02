55626c207058730dcd71a14ca2945ff0fee375dc
arielfmd <ariel.f.m.domingues@gmail.com>
2022-03-02 14:37:49 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/siemens/projectalpha/resources/ClienteResource.java
+++ src/main/java/com/siemens/projectalpha/resources/ClienteResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import com.siemens.projectalpha.domain.Cliente;
 import com.siemens.projectalpha.dto.ClienteDTO;
+import com.siemens.projectalpha.dto.ClienteNewDTO;
 import com.siemens.projectalpha.services.ClienteService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -37,6 +37,15 @@ public class ClienteResource {
         
         Cliente obj = service.find(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+        Cliente obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
     @PutMapping(value="/{id}")
