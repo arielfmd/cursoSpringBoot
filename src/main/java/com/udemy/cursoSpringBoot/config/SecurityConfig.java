855650ee84ce09d2d5ff278a16ab325a0772f24e
arielfmd <ariel.f.m.domingues@gmail.com>
2022-03-23 23:17:51 -0300

    Implementando autorizacao

--- src/main/java/com/udemy/cursoSpringBoot/config/SecurityConfig.java
+++ src/main/java/com/udemy/cursoSpringBoot/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.udemy.cursoSpringBoot.config;
 import java.util.Arrays;
 
 import com.udemy.cursoSpringBoot.security.JWTAuthenticationFilter;
+import com.udemy.cursoSpringBoot.security.JWTAuthorizationFilter;
 import com.udemy.cursoSpringBoot.security.JWTUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
