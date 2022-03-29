6012d4d431595a0285ad90e13d9d94fabcfdabc8
arielfmd <ariel.f.m.domingues@gmail.com>
2022-03-28 21:23:53 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/udemy/cursoSpringBoot/services/PedidoService.java
+++ src/main/java/com/udemy/cursoSpringBoot/services/PedidoService.java
@@ -3,6 +3,7 @@ package com.udemy.cursoSpringBoot.services;
 import java.util.Date;
 import java.util.Optional;
 
+import com.udemy.cursoSpringBoot.domain.Cliente;
 import com.udemy.cursoSpringBoot.domain.ItemPedido;
 import com.udemy.cursoSpringBoot.domain.PagamentoComBoleto;
 import com.udemy.cursoSpringBoot.domain.Pedido;
@@ -10,9 +11,14 @@ import com.udemy.cursoSpringBoot.domain.enums.EstadoPagamento;
 import com.udemy.cursoSpringBoot.repositories.ItemPedidoRepository;
 import com.udemy.cursoSpringBoot.repositories.PagamentoRepository;
 import com.udemy.cursoSpringBoot.repositories.PedidoRepository;
+import com.udemy.cursoSpringBoot.security.UserSS;
+import com.udemy.cursoSpringBoot.services.exceptions.AuthorizationException;
 import com.udemy.cursoSpringBoot.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -66,5 +72,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer size, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, size, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
