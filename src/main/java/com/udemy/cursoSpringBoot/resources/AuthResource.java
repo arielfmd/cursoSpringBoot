afe0e9a2bcbf7989c58639f51a7df6987287cc98
arielfmd <ariel.f.m.domingues@gmail.com>
2022-03-28 22:17:00 -0300

    Esqueci a senha

--- src/main/java/com/udemy/cursoSpringBoot/resources/AuthResource.java
+++ src/main/java/com/udemy/cursoSpringBoot/resources/AuthResource.java
@@ -1,14 +1,18 @@
 package com.udemy.cursoSpringBoot.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
+import com.udemy.cursoSpringBoot.dto.EmailDTO;
 import com.udemy.cursoSpringBoot.security.JWTUtil;
 import com.udemy.cursoSpringBoot.security.UserSS;
+import com.udemy.cursoSpringBoot.services.AuthService;
 import com.udemy.cursoSpringBoot.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +23,20 @@ public class AuthResource {
 	@Autowired
 	private JWTUtil jwtUtil;
 
+    @Autowired
+	private AuthService service;
+
 	@PostMapping(value = "/refresh_token")
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+
+    @PostMapping(value = "/forgot")
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/udemy/cursoSpringBoot/services/EmailService.java
+++ src/main/java/com/udemy/cursoSpringBoot/services/EmailService.java
@@ -2,6 +2,7 @@ package com.udemy.cursoSpringBoot.services;
 
 import javax.mail.internet.MimeMessage;
 
+import com.udemy.cursoSpringBoot.domain.Cliente;
 import com.udemy.cursoSpringBoot.domain.Pedido;
 
 import org.springframework.mail.SimpleMailMessage;
@@ -15,4 +16,6 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 
 	void sendHtmlEmail(MimeMessage msg);
+
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
